1450fe5e99456bafbdec50bff5267136714d3f0e
Iwe-Coumou <i.coumou@outlook.com>
2026-04-01 13:45:51 +0200

    added mode validation to main.c and some more files for later

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World.");
+char *_validate_mode(char *input) {
+  if (strcmp(input, "host") == 0 || strcmp(input, "join") == 0) return input;
+  return NULL;
+}
+
+int main(int argc,char *argv[]) {
+  char *mode = _validate_mode(argv[1]);
+  if (!mode) {
+    fprintf(stderr, "usage: game host\n"
+                    "       game join <ip>\n");
+    exit(1);
+  } else {
+    printf("Running in %s mode...\n", mode);
+  }
 }
---
+++ main.h
@@ -0,0 +1,2 @@
+char  *_validate_mode(char *input);
+int main(int argc, char *argv[]);
